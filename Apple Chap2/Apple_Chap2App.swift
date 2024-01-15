a33bb9be7d384789bf378159f45e19859369b1b2
Yukun Xie <yxie@lcs.on.ca>
2024-01-15 12:01:59 -0500

    Initial Commit

---
+++ Apple Chap2/Apple_Chap2App.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Chap2App.swift
+//  Apple Chap2
+//
+//  Created by Yukun Xie on 2024/1/15.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Chap2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple Chap2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Apple Chap2
+//
+//  Created by Yukun Xie on 2024/1/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
